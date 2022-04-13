3d8b04811bd9909893412621e7da37cb3bc055e3
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-04-13 20:58:39 +0900

    ss

--- src/pages/PjDetail.tsx
+++ src/pages/PjDetail.tsx
@@ -1,5 +1,41 @@
 import React from "react";
+import "../css/PjDetail.css";
 
 export const PjDetail = () => {
-  return <div>PjDetail</div>;
+  return (
+    <div>
+      <h2>ECサイトプロジェクト</h2>
+      <button>申請</button>
+      <h2>メンバー</h2>
+      <div className="memberList">
+        <div className="member">
+          <img className="iconImage" src="/1.jpg" alt="" />
+          <div>
+            <p>野口拓也</p>
+            <p>使える技術：react/vue/javascript</p>
+            <p>積極的に共同開発していきたいです！！</p>
+          </div>
+        </div>
+        <div className="member">
+          <img className="iconImage" src="/1.jpg" alt="" />
+          <div>
+            <p>野口拓也</p>
+            <p>使える技術：react/vue/javascript</p>
+            <p>積極的に共同開発していきたいです！！</p>
+          </div>
+        </div>
+        <div className="member">
+          <img className="iconImage" src="/1.jpg" alt="" />
+          <div>
+            <p>野口拓也</p>
+            <p>使える技術：react/vue/javascript</p>
+            <p>積極的に共同開発していきたいです！！</p>
+          </div>
+        </div>
+      </div>
+      <div>募集中！！！</div>
+      <p>＜こんな方を募集中＞</p>
+      <p>reactができる方/Javaで開発したことある方/AWSの知識のある方/</p>
+    </div>
+  );
 };
