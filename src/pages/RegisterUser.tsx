74c94650111f4dddbf14fcc90334d1a3b8fba2f2
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-04-27 16:28:38 +0900

    会員登録ボタンを押すとログイン画面に遷移するように改修

--- src/pages/RegisterUser.tsx
+++ src/pages/RegisterUser.tsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const RegisterUser = () => {
+  const navigate = useNavigate();
+
+  const [langList, setLangList] = useState<Array<string | undefined>>([]);
+  const [langName, setLangName] = useState<string>("");
+
+  const addLang = () => {
+    setLangList((langList) => [...langList, langName]);
+    setLangName("");
+  };
   return (
     <>
       <h1>会員登録</h1>
@@ -21,7 +31,32 @@ export const RegisterUser = () => {
         <input type="text" />
       </div>
       <div>
-        <button>会員登録</button>
+        エンジニア種別：
+        <select name="" id="">
+          <option value="" unselectable="on">
+            --
+          </option>
+          <option value="CL">CL</option>
+          <option value="Web">Web</option>
+          <option value="FR">FR</option>
+        </select>
+      </div>
+      <div>
+        使用可能言語：
+        <input
+          type="text"
+          value={langName}
+          onChange={(e) => setLangName(e.target.value)}
+        />
+        <button onClick={() => addLang()}>追加</button>
+        <ul>
+          {langList.map((langName, index) => {
+            return <li key={index}>{langName}</li>;
+          })}
+        </ul>
+      </div>
+      <div>
+        <button onClick={() => navigate("/Login")}>会員登録</button>
       </div>
     </>
   );
