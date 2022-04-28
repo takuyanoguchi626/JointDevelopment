fabc84263db02f14f32f6f07fcf813edc0c49280
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-04-28 16:43:24 +0900

    aaa

--- src/pages/PjDetail.tsx
+++ src/pages/PjDetail.tsx
@@ -5,7 +5,13 @@ export const PjDetail = () => {
   return (
     <div>
       <h2>ECサイトプロジェクト</h2>
-      <div>プロジェクト説明：簡単なECサイトの開発をしようと考えています。</div>
+      <div>プロジェクト概要：簡単なECサイトの開発をしようと考えています。</div>
+      <div>プロジェクト説明：</div>
+      <pre>
+        開発にあたって、FRはreactかvueでの開発をお願いしたいです。
+        WebはjavaもしくはPHPでの開発経験のある方を募集します。
+        CLはこれらの言語のアプリを運用したことがある方を優先して採用します。
+      </pre>
       <div>発足日：2022年3月25日</div>
       <div>現在の募集状況：募集中！！！</div>
       <div>募集エンジニア：CL,Web,FR</div>
@@ -13,6 +19,7 @@ export const PjDetail = () => {
       <div>活動頻度：週３</div>
       <div>現在参加予定メンバー</div>
       <p>各エンジニアが2人以上集まった段階で締め切ります。</p>
+      <div>現在集まっているエンジニア一覧：</div>
       <div className="memberList">
         <div className="member">
           <img className="iconImage" src="/1.jpg" alt="" />
