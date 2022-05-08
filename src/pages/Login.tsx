aee36b213eb29f0e336cd8515f10f7239fede389
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-05-08 15:52:24 +0900

    ログインのバリデーションチェックを実装

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,20 +1,57 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 export const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  console.log(errors);
+
   return (
     <div>
       <h1>ログイン</h1>
-      <div>
-        メールアドレス：
-        <input type="text" />
-      </div>
-      <div>
-        パスワード：
-        <input type="text" />
-      </div>
-      <div>
-        <button>ログイン</button>
-      </div>
+      <form
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
+      >
+        <div>
+          メールアドレス：
+          <input
+            type="text"
+            {...register("Email", {
+              required: "メールアドレスを入力してください",
+            })}
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                e.preventDefault(); //デフォルトのイベントをプリベント（妨げる）する
+              }
+            }}
+          />
+          <span>{errors.Email?.message}</span>
+        </div>
+        <div>
+          パスワード：
+          <input
+            type="text"
+            {...register("password", {
+              required: "パスワードを入力してください",
+            })}
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                e.preventDefault(); //デフォルトのイベントをプリベント（妨げる）する
+              }
+            }}
+          />
+          <span>{errors.password?.message}</span>
+        </div>
+        <div>
+          <button type="submit">ログイン</button>
+        </div>
+      </form>
     </div>
   );
 };
