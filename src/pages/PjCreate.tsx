68112812d9c5aba7ea7b8999fb6d42dd9a91c0ea
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-05-09 14:35:11 +0900

    プロジェクト作成の入力値をuseFormで管理

--- src/pages/PjCreate.tsx
+++ src/pages/PjCreate.tsx
@@ -1,62 +1,204 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+
+type numberOfKindOfEngineer = {
+  CL: number;
+  Web: number;
+  FR: number;
+  ML: number;
+  QA: number;
+};
 
 export const PjCreate = () => {
   const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm();
+
+  const [numberOfKindOfEngineer, setNumberOfKindOfEngineer] =
+    useState<numberOfKindOfEngineer>({
+      CL: 0,
+      Web: 0,
+      FR: 0,
+      ML: 0,
+      QA: 0,
+    });
+
   return (
     <div>
       <h1>プロジェクト作成</h1>
-      <div>
-        チーム名：
-        <input type="text" />
-      </div>
-      <div>開発概要：簡単なECサイトの開発です。</div>
-      <div>
-        開発内容説明：
-        <textarea name="" id="" cols={30} rows={10}>
-          開発にあたって、FRはreactかvueでの開発をお願いしたいです。
-          WebはjavaもしくはPHPでの開発経験のある方を募集します。
-          CLはこれらの言語のアプリを運用したことがある方を優先して採用します。
-        </textarea>
-      </div>
-      <div>
-        募集エンジニア人数：
+      <form
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
+      >
         <div>
-          CL:
-          <input type="number" />人
+          チーム名：
+          <input
+            placeholder="teamName"
+            type="text"
+            {...register("teamName", {
+              required: "チーム名を入力してください",
+            })}
+          />
         </div>
         <div>
-          Web:
-          <input type="number" />人
+          開発概要：
+          <input
+            placeholder="summary"
+            type="text"
+            {...register("summary", {
+              required: "開発概要を入力してください",
+            })}
+          />
         </div>
         <div>
-          FR:
-          <input type="number" />人
+          <div>開発内容説明：</div>
+          <textarea
+            placeholder="contents"
+            cols={30}
+            rows={10}
+            {...register("contents", {
+              required: "開発内容を入力してください",
+            })}
+          ></textarea>
         </div>
         <div>
-          ML:
-          <input type="number" />人
+          募集エンジニア人数：
+          <div>
+            CL:
+            <input
+              defaultValue={0}
+              type="number"
+              onChange={(e) => {
+                const numberOfKindOfEngineer2 = {
+                  ...{
+                    ...numberOfKindOfEngineer,
+                    CL: Number(e.target.value),
+                  },
+                };
+                setNumberOfKindOfEngineer(() => numberOfKindOfEngineer2);
+                setValue("numberOfKindOfEngineer", numberOfKindOfEngineer2);
+              }}
+            />
+            人
+          </div>
+          <div>
+            Web:
+            <input
+              defaultValue={0}
+              type="number"
+              onChange={(e) => {
+                const numberOfKindOfEngineer2 = {
+                  ...{
+                    ...numberOfKindOfEngineer,
+                    Web: Number(e.target.value),
+                  },
+                };
+                setNumberOfKindOfEngineer(() => numberOfKindOfEngineer2);
+                setValue("numberOfKindOfEngineer", numberOfKindOfEngineer2);
+              }}
+            />
+            人
+          </div>
+          <div>
+            FR:
+            <input
+              defaultValue={0}
+              type="number"
+              onChange={(e) => {
+                const numberOfKindOfEngineer2 = {
+                  ...{
+                    ...numberOfKindOfEngineer,
+                    FR: Number(e.target.value),
+                  },
+                };
+                setNumberOfKindOfEngineer(() => numberOfKindOfEngineer2);
+                setValue("numberOfKindOfEngineer", numberOfKindOfEngineer2);
+              }}
+            />
+            人
+          </div>
+          <div>
+            ML:
+            <input
+              defaultValue={0}
+              type="number"
+              onChange={(e) => {
+                const numberOfKindOfEngineer2 = {
+                  ...{
+                    ...numberOfKindOfEngineer,
+                    ML: Number(e.target.value),
+                  },
+                };
+                setNumberOfKindOfEngineer(() => numberOfKindOfEngineer2);
+                setValue("numberOfKindOfEngineer", numberOfKindOfEngineer2);
+              }}
+            />
+            人
+          </div>
+          <div>
+            QA:
+            <input
+              defaultValue={0}
+              type="number"
+              onChange={(e) => {
+                const numberOfKindOfEngineer2 = {
+                  ...{
+                    ...numberOfKindOfEngineer,
+                    QA: Number(e.target.value),
+                  },
+                };
+                setNumberOfKindOfEngineer(() => numberOfKindOfEngineer2);
+                setValue("numberOfKindOfEngineer", numberOfKindOfEngineer2);
+              }}
+            />
+            人
+          </div>
         </div>
         <div>
-          QA:
-          <input type="number" />人
+          開発期間：
+          <input
+            type="date"
+            {...register("startOfDev", {
+              required: "開発の開始日を入力してください",
+            })}
+          />
+          ~
+          <input
+            type="date"
+            {...register("finishOfDev", {
+              required: "開発の終了日を入力してください",
+            })}
+          />
         </div>
-      </div>
-      <div>
-        開発期間：
-        <input type="date" />~<input type="date" />
-      </div>
-      <div>
-        活動頻度：
-        <select name="" id="">
-          <option value="month">月</option>
-          <option value="week">週</option>
-        </select>
-        <input type="number" />回
-      </div>
-      <div>
-        <button onClick={() => navigate("/PjList")}>プロジェクト作成</button>
-      </div>
+        <div>
+          活動頻度：
+          <select
+            {...register("frequencyUnit", {
+              required: "活動頻度を入力してください",
+            })}
+          >
+            <option value="month">月</option>
+            <option value="week">週</option>
+          </select>
+          <input
+            type="number"
+            {...register("frequencyNumber", {
+              required: "回数を入力してください",
+            })}
+          />
+          回
+        </div>
+        <div>
+          <button type="submit">プロジェクト作成</button>
+        </div>
+      </form>
     </div>
   );
 };
