89fbca650c1ad39f7885bff99694307724e487af
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-05-10 15:30:09 +0900

    ddd

--- src/pages/PjList.tsx
+++ src/pages/PjList.tsx
@@ -5,7 +5,32 @@ import "../css/PjList.css";
 export const PjList = () => {
   return (
     <>
-      <h1>共同開発プロジェクト一覧</h1>
+      <div>
+        <div>
+          エンジニア種別：
+          <select name="" id="">
+            <option value="CL">CL</option>
+            <option value="Web">Web</option>
+            <option value="FR">FR</option>
+            <option value="ML">ML</option>
+            <option value="QA">QA</option>
+          </select>
+        </div>
+        <div>
+          使用言語：
+          <input type="text" placeholder="ex)react vue java" />
+        </div>
+        <div>
+          活動頻度：
+          <select>
+            <option value="month">月</option>
+            <option value="week">週</option>
+          </select>
+          <input type="number" />回
+        </div>
+        <button>検索</button>
+      </div>
+      <h1>メンバー募集一覧</h1>
       <div className="team">
         <Link to={"/PjDetail/"}>
           <h2>チームECサイト</h2>
