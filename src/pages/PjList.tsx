9765165e40b11a9925bd736029320a81742bb8dc
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-05-22 15:35:42 +0900

    PjListのブートストラップが完了

--- src/pages/PjList.tsx
+++ src/pages/PjList.tsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import "../css/PjList.css";
 import { Project } from "../../types/Project";
 import { format } from "date-fns";
+import { Card, Form } from "react-bootstrap";
 
 export const PjList = () => {
   //プロジェクト一覧をDBから取得する
@@ -14,6 +16,15 @@ export const PjList = () => {
   //   };
   //   axiosGet();
   // }, []);
+
+  //useFormの定義
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+    setValue,
+  } = useForm();
 
   const [pjList, setPjList] = useState<Array<Project>>([
     {
@@ -42,56 +53,109 @@ export const PjList = () => {
 
   return (
     <>
-      <div>
-        <div>
-          エンジニア種別：
-          <select name="" id="">
-            <option value="CL">CL</option>
-            <option value="Web">Web</option>
-            <option value="FR">FR</option>
-            <option value="ML">ML</option>
-            <option value="QA">QA</option>
-          </select>
-        </div>
-        <div>
-          開発期間：
-          <input type="date" name="" id="" />~<input type="date" />
-        </div>
-        <div>
-          活動頻度：
-          <select>
-            <option value="month">月</option>
-            <option value="week">週</option>
-          </select>
-          <input type="number" />回
-        </div>
-        <button>検索</button>
-      </div>
-      <h1>メンバー募集一覧</h1>
+      <Card>
+        <Card.Header as="h5">プロジェクト検索</Card.Header>
+        <Card.Body>
+          <form
+            onSubmit={handleSubmit((data) => {
+              // registerUser(data);
 
-      {pjList.map((project, index) => {
-        const start_date = format(project.start_date, "yyyy年MM月dd日");
-        const end_date = format(project.end_date, "yyyy年MM月dd日");
-        return (
-          <div key={project.project_id}>
-            <Link to={`/PjDetail/${project.project_id}`}>
-              <h2>{project.content}</h2>
-              <p>チーム名：{project.team_name}</p>
+              console.log(data);
+            })}
+          >
+            <div>
+              エンジニア種別：
+              {(() => {
+                if (errors.kindOfEngineer?.type === "validate") {
+                  return <span>※エンジニア種別を選択してください</span>;
+                }
+              })()}
+              <Form.Select
+                aria-label="エンジニア種別"
+                {...register("kindOfEngineer", {
+                  validate: (value) => value !== "--",
+                })}
+              >
+                <option value="--" unselectable="on">
+                  --
+                </option>
+                <option value="CL">CL</option>
+                <option value="Web">Web</option>
+                <option value="FR">FR</option>
+                <option value="ML">ML</option>
+                <option value="QA">QA</option>
+              </Form.Select>
+              {/* エンジニア種別：
+            <select name="" id="">
+              <option value="CL">CL</option>
+              <option value="Web">Web</option>
+              <option value="FR">FR</option>
+              <option value="ML">ML</option>
+              <option value="QA">QA</option>
+            </select> */}
+            </div>
+            <div>
+              開発期間：
+              <input type="date" name="" id="" />~<input type="date" />
+            </div>
+            <div>
+              活動頻度：
+              <select>
+                <option value="month">月</option>
+                <option value="week">週</option>
+              </select>
+              <input type="number" />回
+            </div>
+            <button>検索</button>
+          </form>
+        </Card.Body>
+      </Card>
 
-              <p>
-                募集エンジニア：CL({})/Web({})/FR({})/ML({})/QA({})
-              </p>
-              <p>
-                開発期間：{start_date}~{end_date}
-              </p>
-              <p>
-                活動頻度：{project.frequency_day}日/
-                {project.frequency_month_or_week}
-              </p>
-            </Link>
+      <Card>
+        <Card.Header as="h5">メンバー募集一覧</Card.Header>
+        <Card.Body>
+          <div className="PjListContainer">
+            {/* <h1>メンバー募集一覧</h1> */}
+            <div className="pjListMap">
+              {pjList.map((project, index) => {
+                const start_date = format(project.start_date, "yyyy年MM月dd日");
+                const end_date = format(project.end_date, "yyyy年MM月dd日");
+                return (
+                  <Card style={{ width: "18rem" }} key={project.project_id}>
+                    <Link
+                      className="link"
+                      to={`/PjDetail/${project.project_id}`}
+                    >
+                      <Card.Body>
+                        <Card.Title>{project.content}</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">
+                          チーム名：{project.team_name}
+                        </Card.Subtitle>
+                        <hr />
+                        <Card.Text>
+                          募集エンジニア：CL({1})/Web({4})/FR({4})/ML({4})/QA(
+                          {4})
+                          <br />
+                          開発期間：
+                          <br />
+                          {start_date}~{end_date}
+                          <br />
+                          活動頻度：{project.frequency_day}日/
+                          {project.frequency_month_or_week}
+                          <br />
+                        </Card.Text>
+                        {/* <Card.Link href={`/PjDetail/${project.project_id}`}>
+                  Card Link
+                </Card.Link> */}
+                      </Card.Body>
+                    </Link>
+                  </Card>
+                );
+              })}
+            </div>
           </div>
-        );
-      })}
+        </Card.Body>
+      </Card>
     </>
   );
 };
